e935fe7468dacc10d147d384113e0d524f588147
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 13:28:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
     matched_words = Array.new
     @words_array = words_array
     @word = @word.split('').sort.join('')
-    @words_array.each { |word| word.split('').sort.join('') }
+    @words_array.each do |word| word.split('').sort.join('') }
 
   end
 
