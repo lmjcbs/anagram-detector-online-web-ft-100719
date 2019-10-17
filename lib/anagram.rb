29abed806daf96e43b58c2b9a5adc6d01a96b7a2
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 13:31:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
     sorted_word = @word.split('').sort.join('')
     @words_array.each do |word|
       sorted_word = word.split('').sort.join('')
-      matched_words <<  if word == @word
+      matched_words << word if word == @word
     end
     matched_words
   end
